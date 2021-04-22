65a45ff9c3133b14bcb452256a5cfca4b5857019
Antti Juustila <antti.juustila@gmail.com>
2021-04-22 15:53:40 +0300

    Refactored kick forward app

--- BooksAndWords/kick-forward/src/main/java/org/anttijuustila/KickForwardApp.java
+++ BooksAndWords/kick-forward/src/main/java/org/anttijuustila/KickForwardApp.java
@@ -27,47 +27,55 @@ public class KickForwardApp
 {
     public static void main( String[] args )
     {
-        KickForwardApp app = new KickForwardApp();
-        app.run(args[0], args[1], Integer.parseInt(args[2]));
+        if (args.length != 3) {
+            System.out.println("Usage: java -cp target/classes org.anttijuustila.KickForwardApp book-file filter-file count-of-top-words");
+            System.out.println("Example: java -cp target/classes org.anttijuustila.KickForwardApp Bulk.txt filter.txt 25");
+            return;
+        }
+        run(args[0], args[1], Integer.parseInt(args[2]));
     }
 
-    private String stopWordsListFile = null;
-    private int countOfWordsToList = 100;
+    private static String stopWordsListFile = null;
+    private static int countOfWordsToList = 100;
 
     // run > readfile > filterChars > normalize > scan > removeStopWords > frequencies > sort > print > noOp
-    void run(String fileName, String stopWordsFile, int countToList) {
+    static void run(String fileName, String stopWordsFile, int countToList) {
+        System.out.println("\nFile: " + fileName);
+        System.out.println("Filter file: " + stopWordsFile);
+        System.out.println("Listing top " + countToList + " words...\n");
+        long start = System.nanoTime();
         stopWordsListFile = stopWordsFile;
         countOfWordsToList = countToList;
-        readFile(fileName, (t, u) -> {
-            filterChars(t, u);
-        });
+        readFile(fileName, KickForwardApp::filterChars);
+        long duration = System.nanoTime() - start;
+        System.out.format("%n >>> Performance %5.5f secs.%n", duration / 1000000000.0);
     }
 
-    void readFile(String fileName, BiConsumer<String, BiConsumer> filter) {
+    static void readFile(String fileName, BiConsumer<String, BiConsumer> filter) {
         try {
             Path path = Paths.get(fileName);
             String content;
             content = Files.readString(path, StandardCharsets.UTF_8);
-            filter.accept(content, (BiConsumer<String, BiConsumer>) this::normalize);
+            filter.accept(content, (BiConsumer<String, BiConsumer>) KickForwardApp::normalize);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void filterChars(String data, BiConsumer<String, BiConsumer> normalize) {
+    static void filterChars(String data, BiConsumer<String, BiConsumer> normalize) {
         Pattern pattern = Pattern.compile("[\\W_]+");
-        normalize.accept(pattern.matcher(data).replaceAll(" "), (BiConsumer<String, BiConsumer>) this::scan);
+        normalize.accept(pattern.matcher(data).replaceAll(" "), (BiConsumer<String, BiConsumer>) KickForwardApp::scan);
     }
 
-    void normalize(String data, BiConsumer<String, BiConsumer> scanner) {
-        scanner.accept(data.toLowerCase(), (BiConsumer<String[], BiConsumer>) this::removeStopWords);
+    static void normalize(String data, BiConsumer<String, BiConsumer> scanner) {
+        scanner.accept(data.toLowerCase(), (BiConsumer<String[], BiConsumer>) KickForwardApp::removeStopWords);
     }
 
-    void scan(String data, BiConsumer<String [], BiConsumer> remover) {
-        remover.accept(data.split(" "), (BiConsumer<List<String>, BiConsumer>) this::frequencies);
+    static void scan(String data, BiConsumer<String [], BiConsumer> remover) {
+        remover.accept(data.split(" "), (BiConsumer<List<String>, BiConsumer>) KickForwardApp::frequencies);
     }
 
-    void removeStopWords(String [] data, BiConsumer<List<String>, BiConsumer> frequencyCounter) {
+    static void removeStopWords(String [] data, BiConsumer<List<String>, BiConsumer> frequencyCounter) {
         List<String> wordsToFilter = new ArrayList<String>();
         try {
             File file = new File(stopWordsListFile); 
@@ -89,10 +97,10 @@ public class KickForwardApp
                 cleanedWords.add(word);
             }
         }
-        frequencyCounter.accept(cleanedWords, (BiConsumer<Map<String,Integer>, BiConsumer>) this::sort);
+        frequencyCounter.accept(cleanedWords, (BiConsumer<Map<String,Integer>, BiConsumer>) KickForwardApp::sort);
     }
 
-    void frequencies(List<String> words, BiConsumer<Map<String,Integer>, BiConsumer> sorter) {
+    static void frequencies(List<String> words, BiConsumer<Map<String,Integer>, BiConsumer> sorter) {
         Map<String, Integer> wordCounts = new HashMap<>();
 		for (String word : words) {
 			Integer count = wordCounts.get(word);
@@ -102,19 +110,19 @@ public class KickForwardApp
 				wordCounts.put(word, 1);
 			}
 		}
-        sorter.accept(wordCounts, (BiConsumer<Map<String,Integer>, Consumer<Void>>) this::printTop);
+        sorter.accept(wordCounts, (BiConsumer<Map<String,Integer>, Consumer<Void>>) KickForwardApp::printTop);
     }
 
-    void sort(Map<String,Integer> wordFrequencies, BiConsumer<Map<String,Integer>, Consumer<Void>> printer) {
+    static void sort(Map<String,Integer> wordFrequencies, BiConsumer<Map<String,Integer>, Consumer<Void>> printer) {
         Map<String,Integer> topList = new LinkedHashMap<>();
         wordFrequencies.entrySet()
             .stream()
             .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
             .forEachOrdered(x -> topList.put(x.getKey(), x.getValue()));
-        printer.accept(topList, (Consumer<Void>) this::noOp);
+        printer.accept(topList, (Consumer<Void>) KickForwardApp::noOp);
     }
 
-    void printTop(Map<String, Integer> sortedWords, Consumer<Void> noOpFunc) {
+    static void printTop(Map<String, Integer> sortedWords, Consumer<Void> noOpFunc) {
         int counter = 1;
         for (Map.Entry<String,Integer> entry : sortedWords.entrySet()) {
             System.out.format("%4d. %-30s %7d %n", counter++, padRight(entry.getKey(), 30), entry.getValue());
@@ -125,11 +133,11 @@ public class KickForwardApp
 		noOpFunc.accept(null);
 	}
 
-    void noOp(Void v) {
+    static void noOp(Void v) {
         // No implementation.
     }
 
-    private String padRight(String s, int n) {
+    private static String padRight(String s, int n) {
         return String.format("%-" + n + "s", s).replace(' ', '.');  
     }
 }
