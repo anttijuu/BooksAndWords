ac492dedbe83708c100f5a141930c73fcefe8487
Antti Juustila <antti.juustila@gmail.com>
2021-05-22 20:17:05 +0300

    Added parallel version of kick forward using async dispatch queues

---
+++ BooksAndWords/SwiftKickForwardParallel/README.md
@@ -0,0 +1,65 @@
+# Kick Forward Parallel
+
+Kick Forward Parallel demonstrates one programming style to solve the frequent words task, where
+an app reads a text file containing a book. App then calculates the most often used words and their 
+frequencies from a text file, ignoring words listed in another file. 
+
+Result could look like this:
+
+```console
+Listing 100 most common words.
+1. he.................. 32108
+2. that................ 27021
+3. his................. 20040
+4. it.................. 16443
+5. for................. 16186
+6. with................ 16070
+7. was................. 14628
+8. not................. 13568
+9. han................. 13296
+10. him................. 12886
+11. hanen............... 12428
+...
+```
+
+This implementation is demonstrating a variation of the pipeline style, *kick forward* as a way to solve the problem, using Swift as the
+programming language. Additionally, several threads are used to, in parallel, handle the large file in chunks in various async queues. When each of the queues have done their work, they copy their result into a global wordcount storage. This is protected by a semaphore so that only one async thread at a time can write to the global wordcount storage.
+
+Compare this implementation to the synchronous one thread implementation in Kick Forward project. The multiple async task queue is more efficient with large files. Synchronous implementation took 2.59091 secs to handle a file as this parallel implementation took 1.91931 secs. 
+
+Probably with small files the overhead of managing async queues makes the parallel implementation slower. Check out yourself!
+
+The problem solution has been implemented in steps, each step executed in one function. Additionally, each function has, as the last parameter, a function to call next after that function has done it's part of the job.
+
+## Dependencies
+
+Kick Forward Parallel uses the Swift Argument Parser to handle the parameters.
+
+
+## Building and running
+
+Build from the command line:
+
+```console
+swift build -c release
+```
+
+and then run the app:
+
+```console
+./.build/x86_64-apple-macosx/release/kickforward path-to-book.txt path-to-ignore-file.txt 100 
+```
+
+Assuming the binary is in `./.build/x86_64-apple-macosx/release` and user gives the two text files
+and the count of the most frequent words to print out. Text files must be UTF-8 encoded plain text files.
+
+
+## Who did this
+
+The implementation is inspired by the book [Exercises in Programming Style by Cristina Videira Lopes](https://www.routledge.com/Exercises-in-Programming-Style/Lopes/p/book/9780367350208).
+
+
+* (c) Antti Juustila
+* INTERACT Research Group
+* Study program for Information Processing Science
+* University of Oulu, Finland
---
+++ BooksAndWords/SwiftKickForwardParallel/Sources/SwiftKickForwardParallel/main.swift
@@ -0,0 +1,130 @@
+import Foundation
+import ArgumentParser
+
+// Type aliases to make the func declarations easier to read:
+typealias NoOpFunc = () -> Void
+typealias ArrayFunc = (ArraySlice<String>) -> [String: Int]
+typealias ArrayNoOpFunc = (ArraySlice<String>, NoOpFunc) -> [String: Int]
+typealias ArrayArrayNoOpFunc = (ArraySlice<String>, ArrayNoOpFunc) -> [String: Int]
+typealias MapNoOpFunc = ([String: Int], NoOpFunc) -> [String: Int]
+typealias ArrayMapNoOpFunc = ([String], MapNoOpFunc) -> [String: Int]
+typealias ArrayArrayMapNoOpFunc = ([String], ArrayMapNoOpFunc) -> [String: Int]
+
+final class KickForwardParallel: ParsableCommand {
+
+   static let configuration = CommandConfiguration(abstract: "A Swift command-line tool to count the most often used words in a book file.")
+
+   @Argument(help: "The file name for the book file.")
+   private var bookFile: String
+
+   @Argument(help: "The file name for the stop words.")
+   private var stopWordsFile: String
+
+   @Argument(help: "The number of top words to list.")
+   private var topListSize: Int
+
+   var wordsToFilter = [String]()
+
+   func run() throws {
+      print("Book file: \(bookFile).")
+      print("Stop words file: \(stopWordsFile).")
+      print("Listing \(topListSize) most common words.")
+
+      let start = Date()
+
+      // readFile calls filterWords...
+      readFile(file: bookFile, function: filterWords)
+
+      let duration = start.distance(to: Date())
+      print(" >>>> Time \(duration) secs.")
+   }
+
+   func readFile(file: String, function: @escaping ArrayArrayNoOpFunc) -> Void {
+      var data = FileManager.default.contents(atPath: file)
+      var words = [String]()
+      if let data = data {
+         var asString = String(decoding: data, as: UTF8.self)
+         asString = asString.lowercased()
+         words = asString.split{ $0.isWhitespace || $0.isPunctuation }.map{ String($0) }
+      }
+      data = FileManager.default.contents(atPath: stopWordsFile)
+      if let data = data {
+         let asString = String(decoding: data, as: UTF8.self)
+         wordsToFilter = asString.components(separatedBy: ",")
+      }
+
+      let spliceSize = words.count / 4
+      print("spliceSize is \(spliceSize) for \(words.count) words")
+      var map: [String: Int] = [:]
+      print("Starting dispatch queues")
+      let queue = DispatchQueue(label: "com.anttijuustila.kickforward", qos: .userInteractive, attributes: .concurrent)
+      let group = DispatchGroup()
+      let mapSemaphore = DispatchSemaphore(value: 1)
+      // ...filterWords calls calculateFrequencies...
+      for index in stride(from: 0, to: words.count - 1, by: spliceSize) {
+         group.enter()
+         queue.async {
+            let pieces = function(words[index..<min(index+spliceSize,words.count-1)], self.calculateFrequencies)
+            print("Waiting for map...")
+            mapSemaphore.wait()
+            for (key, value) in pieces {
+               if map[key] != nil {
+                  map[key] = map[key]! + value
+               } else {
+                  map[key] = value
+               }
+            }
+            print("...signalling map")
+            mapSemaphore.signal()
+            group.leave()
+         }
+      }
+      print("Waiting for threads...")
+      group.wait()
+      print("Map has before printing \(map.count) elements")
+      printTop(wordCounts: map, function: noOp)
+   }
+
+   func filterWords(words: ArraySlice<String>, function: ArrayNoOpFunc) -> [String: Int] {
+      var cleanedWords = [String]()
+      for word in words {
+         if wordsToFilter.firstIndex(of: word) == nil && !word.isNumeric && word.count >= 2 {
+            cleanedWords.append(word)
+         }
+      }
+      return function(cleanedWords[1...], noOp)
+   }
+
+   func calculateFrequencies(words: ArraySlice<String>, function: NoOpFunc) ->  [String: Int] {
+      var wordCounts = [String: Int]()
+      for word in words {
+         if wordCounts[word] != nil {
+            wordCounts[word]! += 1
+         } else {
+            wordCounts[word] = 1
+         }
+      }
+      return wordCounts
+   }
+
+   func printTop(wordCounts: [String: Int], function: NoOpFunc) -> Void {
+      let sorted = wordCounts.sorted( by: { $0.1 > $1.1 })
+      var counter = 1
+      for (key, value) in sorted {
+         print("\(String(counter).rightJustified(width: 3)). \(key.leftJustified(width: 20, fillChar: ".")) \(value)")
+         counter += 1
+         if counter > topListSize {
+            break
+         }
+      }
+      // ...as printTop calls noOp...
+      function()
+   }
+
+   func noOp() -> Void {
+      // ... it does nothing.
+   }
+
+}
+
+KickForwardParallel.main()
