133a378300ae91e86f1b17119713147e40869727
Antti Juustila <antti.juustila@gmail.com>
2022-02-12 20:46:36 +0200

    Fixed async issues with the help of https://swiftinit.org/articles/argumentparser-async-tutorial and CountLines sample in ArgumentParser source

--- FunctionalParallel/Package.swift
+++ FunctionalParallel/Package.swift
@@ -9,11 +9,9 @@ let package = Package(
       .macOS(.v12)
     ],
     dependencies: [
-      .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
+      .package(url: "https://github.com/apple/swift-argument-parser", .branch("async"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "FunctionalParallel",
             dependencies: [ .product(name: "ArgumentParser", package: "swift-argument-parser") ])
--- FunctionalParallel/Sources/FunctionalParallel/Functional.swift
+++ FunctionalParallel/Sources/FunctionalParallel/Functional.swift
@@ -1,7 +1,13 @@
 import Foundation
+
 import ArgumentParser
 
-struct Functional: ParsableCommand {
+@main
+struct AsyncMain: AsyncMainProtocol {
+   typealias Command = Functional
+}
+
+struct Functional: AsyncParsableCommand {
    static let configuration = CommandConfiguration(abstract: "A Swift command-line tool to count the most often used words in a book file.")
    
    @Argument(help: "The file name for the book file.")
@@ -13,9 +19,8 @@ struct Functional: ParsableCommand {
    @Argument(help: "The number of top words to list.")
    private var topListSize: Int
    
-   init() { }
-   
-   func run() throws {
+
+   mutating func run() async throws {
       print("Book file: \(bookFile).")
       print("Stop words file: \(stopWordsFile).")
       print("Listing \(topListSize) most common words.")
@@ -32,6 +37,7 @@ struct Functional: ParsableCommand {
       let asString = String(decoding: bookData, as: UTF8.self)
       
       // Go through the string, pick words and add them to the array of words.
+
       var words: [String] = []
       var word = String()
       for letter in asString {
@@ -57,57 +63,59 @@ struct Functional: ParsableCommand {
       // async tasks to finish before quitting the app. Otherwise, the console app main thread
       // runs to the end and exits the app before subtasks have finished the word counting.
       let taskSemaphore = DispatchSemaphore(value: 0)
+
       // Calculation of unique word frequencies is done within task group tasks. What is returned is
       // a dictionary of words and their counts from a slice of the array.
       // After getting the results from parallel tasks, the partial results are joined and the dictionary
       // is sorted in descending order and prints out the top words.
       // Count the slice size of the array for the subtasks to handle.
-      let sliceSize = words.count / 8
-      for index in stride(from: 0, to: words.count - 1, by: sliceSize) {
-         // Add tasks to the task group, they are then executed in parallel.
-         let slice = words[index..<min(index+sliceSize,words.count-1)]
+      //      Task(priority: .userInitiated) {
+      // Launch the tasks in a task group.
+      try! await withThrowingTaskGroup(of: [String: Int].self, body: { group in
          // Prepare the unit of asynchronous work in a Task.
-         Task(priority: .userInitiated) {
-            // The word frequencies will be stored in this dictionary.
-            var wordCounts = [String: Int]()
+         // The word frequencies will be stored in this dictionary.
+         var wordCounts = [String: Int]()
 
-            // Launch the tasks in a task group.
-            try! await withThrowingTaskGroup(of: [String: Int].self, body: { group in
-               // Add a task to the task group
-               group.addTask(priority: .userInitiated) { () -> [String: Int] in
-                  // Each task asynchronously count the word frequencies of a slice of an array.
-                  async let result = slice.filter { word in
-                     word.count >= 2 && !wordsToFilter.contains(word)
-                  }.reduce(into: [:]) { counts, word in
-                     counts[word, default: 0] += 1
-                  }
-                  // Return the resulting dictionary from the task to the task group.
-                  return await result
+         let sliceSize = words.count / 8
+         for index in stride(from: 0, to: words.count - 1, by: sliceSize) {
+            // Add tasks to the task group, they are then executed in parallel.
+            let slice = words[index..<min(index+sliceSize,words.count-1)]
+
+            // Add a task to the task group
+            group.addTask(priority: .userInitiated) { () -> [String: Int] in
+               // Each task asynchronously count the word frequencies of a slice of an array.
+               async let result = slice.filter { word in
+                  word.count >= 2 && !wordsToFilter.contains(word)
+               }.reduce(into: [:]) { counts, word in
+                  counts[word, default: 0] += 1
                }
-               // Combine the results from the subtasks as they finish.
-               for try await partial in group {
-                  for (key, value) in partial {
-                     if wordCounts[key] != nil {
-                        wordCounts[key] = wordCounts[key]! + value
-                     } else {
-                        wordCounts[key] = value
-                     }
-                  }
-               }
-            })
-            // Now subtasks have finished and results from those have been combined to wordCounts.
-            // Sort the combined dictionary by the word count (value of the map).
-            var counter = 1
-            wordCounts.sorted(by: { lhs, rhs in
-               lhs.value > rhs.value
-            }).prefix(topListSize).forEach { key, value in
-               print("\(String(counter).rightJustified(width: 3)). \(key.leftJustified(width: 20, fillChar: ".")) \(value)")
-               counter += 1
+               // Return the resulting dictionary from the task to the task group.
+               return await result
             }
-            // Signal that the async tasks are finished.
-            taskSemaphore.signal()
+            // Combine the results from the subtasks as they finish.
+            for try await partial in group {
+               for (key, value) in partial {
+                  if wordCounts[key] != nil {
+                     wordCounts[key] = wordCounts[key]! + value
+                  } else {
+                     wordCounts[key] = value
+                  }
+               }
+            }
          }
-      }
+         // Now subtasks have finished and results from those have been combined to wordCounts.
+         // Sort the combined dictionary by the word count (value of the map).
+         var counter = 1
+         wordCounts.sorted(by: { lhs, rhs in
+            lhs.value > rhs.value
+         }).prefix(topListSize).forEach { key, value in
+            print("\(String(counter).rightJustified(width: 3)). \(key.leftJustified(width: 20, fillChar: ".")) \(value)")
+            counter += 1
+         }
+         // Signal that the async tasks are finished.
+         taskSemaphore.signal()
+      })
+
       // Waiting for the async tasks to finish.
       taskSemaphore.wait()
       
@@ -116,5 +124,4 @@ struct Functional: ParsableCommand {
    }
    
 }
-   
-Functional.main()
+
