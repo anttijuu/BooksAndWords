c25f51af1f68169ba14a59f6eee0be0b52236862
Antti Juustila <antti.juustila@gmail.com>
2022-08-31 12:52:14 +0300

    Using version 1.1.4 from Swift arument parser since async branch no longer there nor needed

--- FunctionalParallel/Package.swift
+++ FunctionalParallel/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
       .macOS(.v12)
     ],
     dependencies: [
-      .package(url: "https://github.com/apple/swift-argument-parser", .branch("async"))
+      .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0") // , .branch("async"))
     ],
     targets: [
         .executableTarget(
